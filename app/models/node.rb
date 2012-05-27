f3d19a49f79c2901043dfc2c7e094ff25c934472
Victor Costan <costan@gmail.com>
2012-05-27 07:03:47 -0400

    Bugfixes.

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,14 +1,14 @@
 # A blob or list.
 class Node < ActiveRecord::Base
   # The first block of the file.
-  belongs_to :block0, class: 'Block'
+  belongs_to :block0, class_name: 'Block'
   validates :block0, presence: true
 
   # The amount of data stored by this node.
   validates :size, numericality: { :greater_than_or_equal_to => 0 }
 
   # The user that can read and write to this node.
-  belongs_to :owner, class: 'User', inverse_of: :nodes
+  belongs_to :owner, class_name: 'User', inverse_of: :nodes
   validates :owner, presence: true
   
   # Block list for blocks with multiple nodes.
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,10 @@ class User < ActiveRecord::Base
   # Admins can mess with the server.
   validates :admin, inclusion: { in: [true, false] }
   
-  # The blocks assigned to a user's files.
+  # The blocks assigned to a user's nodes.
   has_many :blocks, inverse_of: :owner, foreign_key: 'owner_id',
                     dependent: :nullify
+
+  # The nodes (blobs and lists) owned by a user.
+  has_many :nodes, inverse_of: :user, dependent: :destroy
 end
